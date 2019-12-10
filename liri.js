08db7e44d4582c8445a1a61b44690a3e4a3b0303
Bradley Knutson <bradley@bknutson.com>
2019-12-10 10:40:48 -0700

    Added spotify search function

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ function liriINIT(){
             name: 'command',
             message: "Which command would you like to run?",
             type: 'rawlist',
-            choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+            choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says', 'Exit']
         }
     ]).then(response => {
         switch(response.command){
@@ -28,15 +28,50 @@ function liriINIT(){
             case 'do-what-it-says':
                 doWhatItSays();
                 break;
+            case 'Exit':
+                console.log('Goodbye');
+                return;
         }
     }).catch(err => {
         console.log(err);
     });
 }
 
-
-function concertThis(){
-    console.log('hello there');
+function spotifyThisSong(){
+    inquirer.prompt([
+        {
+            name: 'song',
+            message: "What song would you like to search for?",
+            type: 'input',
+            default: 'The Sign - Ace of Base',
+        }
+    ]).then(response => {
+        spotify.search({
+            type: 'track',
+            query: response.song,
+            limit: 1
+        }).then(res => {
+            if(res.tracks.items.length > 0){
+                var song = res.tracks.items[0];
+                var artists = [];
+                song.artists.forEach(artist => {
+                    artists.push(artist.name);
+                });
+                console.log('\n```````````````\nSong Name: ' + song.name + "\nArtist(s): " + artists.toString() + "\nPreview Link: " + song.preview_url + " \nAlbum: " + song.album.name + "\n\n```````````````\n");
+                liriINIT();
+            }else{
+                console.log('That does not return any results');
+                liriINIT();
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+    }).catch(err => {
+        console.log(err);
+    });
 }
+
+
+
 
 liriINIT();
