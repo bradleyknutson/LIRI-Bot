cf5e596999d68743ee797e3c6ae29b488d2ef1e5
Bradley Knutson <bradley@bknutson.com>
2019-12-10 09:31:22 -0700

    Added initial inquirer to choose a command

--- liri.js
+++ liri.js
@@ -6,3 +6,37 @@ var axios = require('axios');
 var keys = require('./keys.js');
 var spotify = new Spotify(keys.spotify);
 
+function liriINIT(){
+    inquirer.prompt([
+        {
+            name: 'command',
+            message: "Which command would you like to run?",
+            type: 'rawlist',
+            choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+        }
+    ]).then(response => {
+        switch(response.command){
+            case 'concert-this':
+                concertThis();
+                break;
+            case 'spotify-this-song':
+                spotifyThisSong();
+                break;
+            case 'movie-this':
+                movieThis();
+                break;
+            case 'do-what-it-says':
+                doWhatItSays();
+                break;
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+
+function concertThis(){
+    console.log('hello there');
+}
+
+liriINIT();
